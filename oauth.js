d192f5e4eada065160b2dcc5ca35b6a4e1c491a5
dhirender <dhirender.tyagi@gmail.com>
2016-02-08 15:13:43 +0530

    changes in .gitignore and package.json

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+  facebook: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+  }
+};
+
+module.exports = ids;
