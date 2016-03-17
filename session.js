9a70df58fda25ef8f83d379cc63e33a934afa91f
dhirender <dhirender.tyagi@gmail.com>
2016-03-17 15:16:46 +0530

    changes for session with express

--- session.js
+++ session.js
@@ -4,17 +4,18 @@ var bodyParser = require('body-parser');
 var app = express();
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var RedisStore = require('connect-redis')(session);
+// var RedisStore = require('connect-redis')(session);
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
+mongoose.connect('mongodb://localhost/session-example');
+
 app.set('views', __dirname + '/views');
 
 app.use(cookieParser());
 app.use(session({
-    store: new RedisStore({
-        host: '127.0.0.1',
-        port: 6379
-    }),
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
     secret: 'Its a secret.',
-    cookie: { secure: true }
+    cookie: { secure: false }
 })); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,10 +24,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 var sess;
 
 app.get('/',function(req,res){
+
+	// New session will get created
 //var cookies = new Cookies(req,res,keys);
     temp = req.cookies;
-    console.log("cookie work is " + JSON.stringify(temp));
-	sess=req.session;
+    console.log("cookie work is " + temp["connect.sid"]);
+	sess= req.session;
+
 	console.log("session is " +JSON.stringify(sess));
 	//Session set when user Request our app via URL
 	if(sess && sess.email)
@@ -39,55 +43,59 @@ app.get('/',function(req,res){
 		res.redirect('/admin');
 	}
 	else{
-		res.send('no session found');
+		res.write('<h1>no session found. Please login first.</h1>');
+		res.end('<a href="/login">Login</a>');
 	}
 });
 
 app.get('/login',function(req,res){
 	
+	//cookies to be set
 	res.cookie('email', 'abc@xyz.com', { expires: new Date(new Date().getTime() + (1000 * 60 * 60 * 24)), httpOnly: true });
 	
-sess=req.session;
- req.session.save(function(err){
-   console.log('saved');
- })
+	//get the object of session saved in mongodb
+	sess=req.session;
+	sess_id = sess.id;
+	req.session.save(function(err){
+	   console.log('saved');
+	})
 	//In this we are assigning email to sess.email variable.
 	//email comes from HTML page.
 	sess.email='abc@xyz.com';
+	//update session object in mongodb
 	sess.save(function(err){
        console.log('saved');
     })
 	
 	console.log('session is successfully created');
-	res.end('done');
+	res.write('<h1>Goto admin.</h1>');
+	res.end('<a href="/admin">Admin</a>');
+	
 });
 
 app.get('/admin',function(req,res){
-	sess=req.session;
+	sess=req.cookies;
 	if(sess.email)
 	{
 		res.write('<h1>Hello '+sess.email+'</h1>');
-		res.end('<a href="+">Logout</a>');
+		res.end('<a href="/logout">Logout</a>');
 	}
 	else
 	{
 		res.write('<h1>Please login first.</h1>');
-		res.end('<a href="+">Login</a>');
+		res.end('<a href="/login">Login</a>');
 	}
 
 });
 
 app.get('/logout',function(req,res){
-
-	req.session.destroy(function(err){
-		if(err){
-			console.log(err);
-		}
-		else
-		{
-			res.redirect('/');
-		}
-	});
+	console.log("session was  " + JSON.stringify(req.cookies))
+	sess = req.session;
+	//delete the session
+	sess.destroy(function(err){
+		res.cookies = {};
+		res.redirect('/');
+	})
 
 });
 app.listen(3000,function(){
